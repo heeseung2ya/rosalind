f3ddfd10ad189faf0a65e3d170371340acf89522
monica6549 <monica6549@hanmail.net>
2021-07-06 01:15:48 +0900

    add DNA.py

---
+++ DNA.py
@@ -0,0 +1,11 @@
+data = './rosalind_dna_7_dataset.txt'
+
+base = ['A', 'C', 'G', 'T']
+base_cnt = [0, 0, 0, 0]
+
+with open(data, 'r') as db:
+    for line in db.readlines():
+        for b in base:
+            base_cnt[base.index(b)] += line.count(b)
+for i in base_cnt:
+    print(i, end=' ')
