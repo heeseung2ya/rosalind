18c9b96a696d059814e2372bcc60a8e62768e7b0
heeseung2ya <69632673+heeseung2ya@users.noreply.github.com>
2021-09-26 21:36:36 +0900

    2021.09.26

--- code/GC.py
+++ code/GC.py
@@ -1,3 +1,39 @@
+# 2021년 9월 26일 업데이트
+
+# import sys
+filename = "rosalind_gc.txt"
+# filename = sys.argv[1]
+
+d_DNA = {}
+d_GC = {}
+header = ""
+read_file = open(filename, "r")
+for line in read_file:
+    line = line.strip()
+
+    if line.startswith(">"):
+        header = line[1:]
+        d_DNA[header] = ""
+    else:
+        d_DNA[header] += line
+
+
+for key, value in d_DNA.items():
+    d_GC[key] = (value.count("G") + value.count("C")) / len(value) * 100
+
+for key, value in d_GC.items():
+    if value == max(d_GC.values()):
+        max_key = key
+        max_value = value
+
+print(max_key)
+print(f"{max_value:.6f}")
+
+
+
+
+'''
+# 이전 코드
 data = 'rosalind_gc.txt' # path of data
 
 d_seq = {}
@@ -19,3 +55,4 @@ for k, v in d_seq.items():
     if max(d_seq.values()) == v:
         print(k)
         print(v)
+'''
