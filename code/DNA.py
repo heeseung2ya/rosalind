89f4f411e0c18788d19eba61367aa158b933e4c0
heeseung2ya <69632673+heeseung2ya@users.noreply.github.com>
2021-10-10 01:12:08 +0900

    Update 2021.10.10.

--- code/DNA.py
+++ code/DNA.py
@@ -1,10 +1,16 @@
 filename = "../rosalind_dna.txt"
 fr = open(filename, "r")
+
+cnt_A = 0
+cnt_C = 0
+cnt_G = 0
+cnt_T = 0
+
 for line in fr:
     line = line.strip()
-    cnt_A = line.count("A")
-    cnt_C = line.count("C")
-    cnt_G = line.count("G")
-    cnt_T = line.count("T")
+    cnt_A += line.count("A")
+    cnt_C += line.count("C")
+    cnt_G += line.count("G")
+    cnt_T += line.count("T")
 
 print(cnt_A, cnt_C, cnt_G, cnt_T)
